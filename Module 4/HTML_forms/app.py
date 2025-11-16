a8ded5d8aa9f0b2cbbdf085ada97e557b226937f
BoVlad <minecraft@karnyz.com>
2025-11-16 14:00:15 +0200

    1

--- Module 4/HTML_forms/app.py
+++ Module 4/HTML_forms/app.py
@@ -1,20 +1,26 @@
 from flask import *
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sos1so4ka4uZ*&Tg8#d0v!9v@zq3x'
 
-# Маршрут для списку постів
+
 @app.get('/contact')
 def contact_form():
-    return render_template("contact_form.html")
-    # render_template: Функція для рендерингу HTML-шаблонів.
+    form = LoginForm()
+    return render_template("contact_form.html", form=form)
+
     
 @app.post('/submit_feedback')
 def submit_feedback_form():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    message = request.form.get('message')
-    return jsonify([name, email, message])
+    form = LoginForm()
+    name = form.name.data
+    email = form.email.data
+    message = form.message.data
+    # message = request.form.get('message')
+    if not form.validate_on_submit():
+        return jsonify({'errors': form.errors}), 400
+    return jsonify({"name": name, "email": email, "message": message})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Module 4/HTML_forms/forms.py
+++ Module 4/HTML_forms/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+import email_validator
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
-    submit = TextAreaField('Повідомлення', validators=[DataRequired(), Length(min=50, max=1024)])
+    name = StringField("Ваше Ім'я", validators=[DataRequired(), Length(min=1, max=32, message="Ім'я має бути від 1 до 32 символів")])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Неправильній email")])
+    message = TextAreaField('Повідомлення', validators=[DataRequired(), Length(min=50, max=1024, message="Повідомлення має бути від 50 до 1024 символів")])
+    submit = SubmitField('Увійти')
     # FlaskForm: Базовий клас для всіх форм.
     # StringField, PasswordField, SubmitField: Типи полів форми.
     # validators: Список правил для перевірки.
